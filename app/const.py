7c31dd8fdc183bd91922c44b07f39d129389d04a
Mateusz Rynkiewicz <rynkiewiczmate@gmail.com>
2023-06-10 14:04:52 +0200

    Fix RESULTS_FOLDER variable

--- app/const.py
+++ app/const.py
@@ -3,6 +3,6 @@ from pathlib import Path
 BST_PATH = Path("BST/")
 CONF_NAME = "conf.yaml"
 
-RESULTS_FOLDER = Path("RESULTS/")
+RESULTS_FOLDER = Path("results/")
 RESULT_JSON = RESULTS_FOLDER / "results.json"
 CONF_JSON = RESULTS_FOLDER / "conf.json"
