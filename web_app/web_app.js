17591472952d0d9e67fadbb1cc9bb09d33905189
Mateusz Rynkiewicz <rynkiewiczmate@gmail.com>
2023-06-13 11:52:53 +0200

    Add conf.json

--- web_app/web_app.js
+++ web_app/web_app.js
@@ -12,72 +12,83 @@ function transformData(data, mode) {
 let charts = [];
 
 
-function createPlot(element_id, title, mode) {
-    fetch('results.json')
-        .then(response => response.json())
-        .then(data => {
-            const ctx = document.getElementById(element_id).getContext('2d');
-            const datasets = transformData(data, mode);
-            const lineChart = new Chart(ctx, {
-                type: 'line',
-                data: {
-                    datasets: datasets
+function createPlot(data, element_id, title, mode) {
+
+    const ctx = document.getElementById(element_id).getContext('2d');
+    const datasets = transformData(data, mode);
+    const lineChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            datasets: datasets
+        },
+        options: {
+            plugins: {
+                legend: {
+                    onClick: function (e, legendItem) {
+                        const index = legendItem.datasetIndex;
+                        const ci = this.chart;
+
+                        // Toggle the visibility of this dataset
+                        ci.getDatasetMeta(index).hidden = ci.isDatasetVisible(index) ? true : false;
+
+                        // Now loop over the other charts and do the same
+                        charts.forEach((chart) => {
+                            if (chart !== ci) {
+                                const otherMeta = chart.getDatasetMeta(index);
+                                otherMeta.hidden = ci.getDatasetMeta(index).hidden;
+                                chart.update();
+                            }
+                        });
+                        ci.update();
+                    },
                 },
-                options: {
-                    plugins: {
-                        legend: {
-                            onClick: function (e, legendItem) {
-                                const index = legendItem.datasetIndex;
-                                const ci = this.chart;
-
-                                // Toggle the visibility of this dataset
-                                ci.getDatasetMeta(index).hidden = ci.isDatasetVisible(index) ? true : false;
-
-                                // Now loop over the other charts and do the same
-                                charts.forEach((chart) => {
-                                    if (chart !== ci) {
-                                        const otherMeta = chart.getDatasetMeta(index);
-                                        otherMeta.hidden = ci.getDatasetMeta(index).hidden;
-                                        chart.update();
-                                    }
-                                });
-                                ci.update();
-                            },
-                        },
-                        title: {
-                            display: true,
-                            text: title,
-                        },
+                title: {
+                    display: true,
+                    text: title,
+                },
+            },
+            interaction: {
+                intersect: false,
+                mode: 'index',
+            },
+            scales: {
+                x: {
+                    title: {
+                        display: true,
+                        text: 'Amount of elements'
                     },
-                    interaction: {
-                        intersect: false,
-                        mode: 'index',
+                    beginAtZero: true,
+                    type: 'linear',
+                },
+                y: {
+                    title: {
+                        display: true,
+                        text: "Time (s)",
                     },
-                    scales: {
-                        x: {
-                            title: {
-                                display: true,
-                                text: 'Amount of elements'
-                            },
-                            beginAtZero: true,
-                            type: 'linear',
-                        },
-                        y: {
-                            title: {
-                                display: true,
-                                text: "Time (s)",
-                            },
-                            beginAtZero: true,
-                        }
-                    }
+                    beginAtZero: true,
+                    type: 'linear',
                 }
-            });
-            charts.push(lineChart);
-        });
+            }
+        }
+    });
+    charts.push(lineChart);
 }
 
-createPlot('addingChart', 'Adding elements to BST', "add");
-createPlot('checkingChart', 'Checking elements in BST', "check");
-createPlot('lenChart', 'Counting elements in BST', "len");
-createPlot('heightChart', 'Counting height of BST', "height");
+function main2(results, conf) {
 
+    createPlot(results, 'addingChart', 'Adding elements to BST', "add");
+    createPlot(results, 'checkingChart', 'Checking elements in BST', "check");
+    createPlot(results, 'lenChart', 'Counting elements in BST', "len");
+    createPlot(results, 'heightChart', 'Counting height of BST', "height");
+}
+
+function main() {
+    Promise.all([
+        fetch('results/results.json').then(response => response.json()),
+        fetch('results/conf.json').then(response => response.json())
+    ]).then(([results, conf]) => {
+        main2(results, conf);
+    });
+}
+
+main();
